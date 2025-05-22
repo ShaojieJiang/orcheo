3169d6e37e120af85f5caf3f04828c9e358e6d80
ShaojieJiang <shaojie.jiang1@gmail.com>
2025-05-22 16:56:07 +0800

    Implement structured output config

--- src/aic_flow/nodes/ai.py
+++ src/aic_flow/nodes/ai.py
@@ -1,14 +1,38 @@
 """AI Agent node."""
 
+import json
 from dataclasses import dataclass
 from typing import Any
 from langchain.chat_models import init_chat_model
 from langchain_core.runnables import RunnableConfig
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.prebuilt import create_react_agent
+from pydantic import BaseModel
+from typing_extensions import Literal
 from aic_flow.graph.state import State
 from aic_flow.nodes.base import AINode
 from aic_flow.nodes.registry import NodeMetadata, registry
+
+
+class StructuredOutput(BaseModel):
+    """Structured output config for the agent."""
+
+    type: Literal["json_schema", "json_dict", "pydantic", "typed_dict"]
+    schema: str
+
+    def get_schema_type(self) -> dict | BaseModel:
+        """Get the schema type based on the schema type and content."""
+        if self.type == "json_schema":
+            return json.loads(self.schema)
+        else:
+            # Execute the schema as Python code and get the last defined object
+            namespace = {}
+            schema = (
+                "from pydantic import BaseModel\nfrom typing_extensions import TypedDict\n"
+                + self.schema
+            )
+            exec(schema, namespace)
+            return list(namespace.values())[-1]
 
 
 @registry.register(
@@ -26,12 +50,8 @@ class Agent(AINode):
     system_prompt: str | None = None
     checkpointer: str | None = None
     # tools: list[BaseTool] # TODO: Add tools and MCP support
-    structured_output: Any | None = None
-    """Structured output for the agent.
-
-    Supports all that supported by LangGraph, but currently, only supports
-    JSON Schema.
-    """
+    structured_output: dict | StructuredOutput | None = None
+    """Structured output for the agent."""
 
     async def run(self, state: State, config: RunnableConfig) -> dict[str, Any]:
         """Execute the agent and return results."""
@@ -50,11 +70,19 @@ class Agent(AINode):
             case _:
                 raise ValueError(f"Invalid checkpointer: {self.checkpointer}")
 
+        if type(self.structured_output) is dict:
+            structured_output = StructuredOutput(**self.structured_output)
+        response_format = (
+            None
+            if self.structured_output is None
+            else structured_output.get_schema_type()
+        )
+
         agent = create_react_agent(
             model,
             tools=[],
             prompt=self.system_prompt,
-            response_format=self.structured_output,
+            response_format=response_format,
             checkpointer=checkpointer,
         )
 
