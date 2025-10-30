38b4c81f3e4f9819fd6f571b8ff47a81b8a7b947
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-10-30 08:57:41 +0100

    Change default SQLite paths

--- src/orcheo/config.py
+++ src/orcheo/config.py
@@ -13,7 +13,7 @@ VaultBackend = Literal["inmemory", "file", "aws_kms"]
 
 _DEFAULTS: dict[str, object] = {
     "CHECKPOINT_BACKEND": "sqlite",
-    "SQLITE_PATH": "checkpoints.sqlite",
+    "SQLITE_PATH": "~/.orcheo/checkpoints.sqlite",
     "REPOSITORY_BACKEND": "sqlite",
     "REPOSITORY_SQLITE_PATH": "~/.orcheo/workflows.sqlite",
     "CHATKIT_SQLITE_PATH": "~/.orcheo/chatkit.sqlite",
@@ -24,7 +24,7 @@ _DEFAULTS: dict[str, object] = {
     "PORT": 8000,
     "VAULT_BACKEND": "file",
     "VAULT_ENCRYPTION_KEY": None,
-    "VAULT_LOCAL_PATH": ".orcheo/vault.sqlite",
+    "VAULT_LOCAL_PATH": "~/.orcheo/vault.sqlite",
     "VAULT_AWS_REGION": None,
     "VAULT_AWS_KMS_KEY_ID": None,
     "VAULT_TOKEN_TTL_SECONDS": 3600,
